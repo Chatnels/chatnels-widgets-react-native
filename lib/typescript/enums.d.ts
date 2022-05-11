492881df5f50f6f5b01c263e9422608b5049be9c
Justin Lee <justin.lee@chatnels.com>
2022-05-11 14:27:55 -0700

    updated enums

--- lib/typescript/enums.d.ts
+++ lib/typescript/enums.d.ts
@@ -1,6 +1,13 @@
 export declare enum ChatnelsEventsType {
+    EXTERNAL_URL = "external:url",
+    INBOX_BACK = "inbox:back",
+    INBOX_ITEM_CLICKED = "inbox:item:clicked",
+    REAUTH = "reAuth",
+    THREADS_BACK = "threads:back",
+    USER_PROFILE_CLICK = "user:profile:click"
+}
+export declare enum InternalChatnelsEventsType {
     LOAD_SCRIPT_ERROR = "loadScriptError",
     APP_READY = "app:ready",
-    APP_REQUEST_FOCUS = "app:request:focus",
-    EXTERNAL_URL = "external:url"
+    APP_REQUEST_FOCUS = "app:request:focus"
 }
